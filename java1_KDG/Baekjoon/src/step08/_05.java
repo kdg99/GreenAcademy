91e37070d5d5ea9b9360b9fdd3d3ae1fd8070210
KimDongGeun200418 <chie44911@gmail.com>
2022-10-17 13:52:52 +0900

    2022.10.17.fromGA

--- java1_KDG/Baekjoon/src/step08/_05.java
+++ java1_KDG/Baekjoon/src/step08/_05.java
@@ -5,18 +5,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /*
- * 날짜 : 2022/10/05
+ * 날짜 : 2022/10/17
  * 이름 : 김동근
- * 내용 : 백준 난이도8 1번 문제
+ * 내용 : 백준 난이도8 5번 문제
  */
 public class _05 {
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = 1;
+		int count = 0;
+		while(n != 0){
+			n = Integer.parseInt(br.readLine()); 
+			count = 0;
+			if(n == 0) break;
+			
+			for(int i=n+1 ; i<=2*n ; i++) {
+				if(isPrime(i)) {
+					count++;
+				}
+			}
+			System.out.println(count);
+		}
+		
 		
 		
 		br.close();
 	} //main-end
+	
+	public static boolean isPrime(int num){
+        if(num < 2) return false;
+        for(int i=2; i*i<=num; i++){
+            if(num % i == 0) return false;            
+        }
+        return true;
+    }
 
 }
